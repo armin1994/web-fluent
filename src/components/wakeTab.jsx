bb87e5cadb2be31584d471f6897a58547d3f5774
armin <ayoub.benachour@esprit.tn>
2018-04-20 00:30:47 +0100

    funal push

--- src/components/wakeTab.jsx
+++ src/components/wakeTab.jsx
@@ -5,24 +5,6 @@ export default class WakeTab extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      username: "",
-      password: ""
-    };
-  }
-
-  validateForm() {
-    return this.state.username.length > 0 && this.state.password.length > 0;
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    const { username, password } = this.state;
-  }
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
   }
 
   render() {
